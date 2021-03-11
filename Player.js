1d6fa6913d3c72758eeb94aea627dd399a6f8506
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2021-03-11 09:33:44 -0500

    More work on the music page.

--- Player.js
+++ Player.js
@@ -1,131 +1,44 @@
-var cSongIndex = 0;
-var gSongIndex = 0;
+playMode = true;
+songIndex = 0;
+playing = false;
 
-var maxwidth = window.outerWidth;
+var player = document.querySelector("#player");
+var source = document.querySelector("#audioSource");
+var songName = document.querySelector("#songName");
+var songInst = document.querySelector("#instruments");
+var volumeControl = document.querySelector("#volControl");
+volumeControl.max = 100;
+volumeControl.min = 0;
+var timeControl = document.querySelector("#time");
+var songDuration;
+timeControl.max = songDuration;
+timeControl.min = 0;
 
-var cPlaying = false;
-var gPlaying = false;
+chooseSong(songIndex);
 
-chooseCSong(cSongIndex);
-chooseGSong(gSongIndex);
-
-var compositionPlayer;
-var cSource;
-var garagePlayer;
-var gSource;
-
-window.onload = function () {
-    compositionPlayer = document.getElementById("compositionPlayer");
-    cSource = document.getElementById("cSource");
-    console.log(compositionPlayer);
-    console.log(cSource);
-    console.log(garagePlayer);
-    console.log(gSource);
+function nextSong(){
+    songIndex+=1;
+    chooseSong(songIndex);
 }
 
-function nextCSong() {
-    chooseCSong(cSongIndex += 1);
+function prevSong(){
+    songIndex-=1;
+    chooseSong(songIndex);
 }
 
-function nextGSong() {
-    chooseGSong(gSongIndex += 1);
-}
-
-function prevCSong() {
-    chooseCSong(cSongIndex -= 1);
-}
-
-function prevGSong() {
-    chooseGSong(gSongIndex -= 1);
-}
-
-function chooseCSong(n) {
-    if (n > 4) {
-        n = 0;
+function chooseSong(n){
+    if (playMode){
+        if (n>4){
+            n = 0;
+            songIndex = 0;
+        }
+        else if (n<0){
+            n = 4;
+            songIndex = 4;
+        }
+        if (n==0){
+            source.src = "https://IDGitHubMan.github.io/Audio/Composition/[2 Little Whos] .wav";
+            songName.textContent = "[2 Little Whos]";
+        }
     }
-    else if (n < 0) {
-        n = 4;
-    }
-    if (n == 0) {
-        cSource.src = "https://IDGitHubMan.github.io/Audio/Composition/[2 Little Whos] .wav"
-    }
-    else if (n == 1) {
-        cSource.src = "https://IDGitHubMan.github.io/Audio/Composition/Betrayal.wav"
-    }
-    else if (n == 2) {
-        cSource.src = "https://IDGitHubMan.github.io/Audio/Composition/Enigma.wav"
-    }
-    else if (n == 3) {
-        cSource.src = "https://IDGitHubMan.github.io/Audio/Composition/Foreboding.wav"
-    }
-    else if (n == 4) {
-        cSource.src = "https://IDGitHubMan.github.io/Audio/Composition/New Lands.wav"
-    }
-    compositionPlayer.load();
-}
-
-function chooseGSong(n) {
-    if (n > 12) {
-        n = 0;
-    }
-    else if (n < 0) {
-        n = 12;
-    }
-    if (n == 0) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Battle in the Stars.wav"
-    }
-    else if (n == 1) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Boss Battle.wav"
-    }
-    else if (n == 2) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/City of the Future.wav"
-    }
-    else if (n == 3) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Dreams of the Stars.wav"
-    }
-    else if (n == 4) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Galaxies.wav"
-    }
-    else if (n == 5) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/High Speed Chase.wav"
-    }
-    else if (n == 6) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Hope in Times of Despair.wav"
-    }
-    else if (n == 7) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Hope in Times of Despair Remake.wav"
-    }
-    else if (n == 7) {
-        gSource.src = "https://IDGitHubMan.github.io/Audio/Garageband/Night.wav"
-    }
-    garagePlayer.load();
-}
-
-function playCSong() {
-    if (cPlaying) {
-        compositionPlayer.pause();
-    }
-    else {
-        compositionPlayer.load();
-        compositionPlayer.play();
-    }
-    cPlaying = !cPlaying
-}
-
-function playGSong() {
-    if (gPlaying) {
-        garagePlayer.pause();
-    }
-    else {
-        garagePlayer.load();
-        garagePlayer.play();
-    }
-    gPlaying = !gPlaying;
-}
-
-window.addEventListener('resize', setWidth);
-
-function setWidth() {
-  maxwidth = window.outerWidth;
-  showSlides(slideIndex);
 }
